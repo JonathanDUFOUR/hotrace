d27ecff74de2e44e2285f031a5a4036f82d0da36
jodufour <jodufour@student.42.fr>
2021-12-11 11:41:09 +0100

    Rename __error to __error_quit because of problems on MacOS [OK]

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 22:14:42 by jodufour          #+#    #+#             */
-/*   Updated: 2021/12/11 03:39:59 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/12/11 11:38:54 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include "hotrace.h"
 #include "t_store.h"
 
-static int	__error(t_store *const db)
+static int	__error_quit(t_store *const db)
 {
 	char const	*str = strerror(errno);
 
@@ -33,9 +33,9 @@ int	main(void)
 	t_store *const	db = (t_store *)hr_calloc(1, sizeof(t_store));
 
 	if (!db)
-		return (__error(NULL));
+		return (__error_quit(NULL));
 	if (hr_store(db) || hr_search(db))
-		return (__error(db));
+		return (__error_quit(db));
 	hr_clear_data(db);
 	return (EXIT_SUCCESS);
 }
